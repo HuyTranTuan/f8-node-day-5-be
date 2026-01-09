603f728143a3d6dfa292cdfa45d85f11dfc6f2e9
Huy Tran <trantuanhuyltv@gmail.com>
2026-01-09 20:49:23 +0700

    fix file name

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const authRoute = require("./auth.route");
-const conversationRoute = require("./conversation.route");
-const userRoute = require("./user.route");
+const conversationRoute = require("./conversations.route");
+const userRoute = require("./users.route");
 
 router.use("/auth", authRoute);
 router.use("/conversations", conversationRoute);
